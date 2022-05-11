9f253e9776ae6611073f6a6aa80a72f3d3b538ca
viktoryiaz <vikki_smile94@mail.ru>
2022-05-11 14:33:32 +0300

    add hw 4

---
+++ HW_4.3.js
@@ -0,0 +1,10 @@
+//  Реализуйте считывание из JSONки из файла task2.json с помощью, например, модуля fs. 
+// для дальнейшего использования в функции, описанной в задании
+
+
+const fs = require("fs");
+const users = JSON.parse(fs.readFileSync("./HW_4.2.json"));
+
+//
+let chekid = Array.from(new Set(users.map(item => JSON.stringify(item)))).map(item => JSON.parse(item));
+console.log(chekid);
